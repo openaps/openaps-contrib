aa4576309fbdab3c16302eb87f9484a20be395e4
Ben West <bewest@gmail.com>
2016-05-30 14:37:01 -0700

    tweak trailing comma

--- setup.py
+++ setup.py
@@ -35,5 +35,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=requires,
-    namespace_packages=['openapscontrib'],
+    namespace_packages=['openapscontrib']
 )
