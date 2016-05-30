d004ffb8f70748d5827a0d83697688a729a7dc11
Ben West <bewest@gmail.com>
2016-05-30 14:36:14 -0700

    release openaps-contrib 0.0.13

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requires = ['recurrent']
 
 setup(
     name='openaps-contrib',
-    version='0.0.12',
+    version='0.0.13',
     url='http://github.com/openaps/openaps-contrib',
     download_url='http://pypi.python.org/pypi/openaps-contrib',
     license='MIT',
